e45e98946adeff5a00304c9b7e1933b6600d4903
alex <17674745183@163.com>
2021-10-16 10:34:23 +0800

    新增接口： 1.增加区域(addArea) 2.修改区域(modifyArea)

--- src/main/java/com/al/o2o/service/AreaService.java
+++ src/main/java/com/al/o2o/service/AreaService.java
@@ -1,5 +1,6 @@
 package com.al.o2o.service;
 
+import com.al.o2o.dto.AreaExecution;
 import com.al.o2o.entity.Area;
 
 import java.util.List;
@@ -9,9 +10,29 @@ import java.util.List;
  */
 public interface AreaService {
     public static final String AREALISTKEY = "areaList";
+
     /**
-     * 查询区域列表
-     * @return
+     * 查询区域列表,优先从缓存中获取
+     *
+     * @return 所有区域信息
      */
     List<Area> getAreaList();
+
+    /**
+     * 增加区域信息
+     *
+     * @param area 区域信息
+     * @return 状态标识
+     */
+    AreaExecution addArea(Area area);
+
+    /**
+     * 修改区域信息
+     *
+     * @param area 区域信息
+     * @return 状态标识
+     */
+    AreaExecution modifyArea(Area area);
+
+
 }
