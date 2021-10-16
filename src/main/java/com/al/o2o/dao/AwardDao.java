585fdaef92973893b30574555425938662f6d344
alex <17674745183@163.com>
2021-10-16 11:00:13 +0800

    格式化代码，增加注释

--- src/main/java/com/al/o2o/dao/AwardDao.java
+++ src/main/java/com/al/o2o/dao/AwardDao.java
@@ -10,22 +10,24 @@ import java.util.List;
  * @author Xiahuicheng
  * @PackageName:com.al.o2o.dao
  * @InterfaceName:AwardDao
- * @Description 奖品
+ * @Description 奖品Repository接口
  * @date2021/8/19 10:29
  */
 public interface AwardDao {
     /**
      * 分页查询奖品信息
+     *
      * @param awardCondition 奖品信息
-     * @param rowIndex 从第几页开始
-     * @param pageSize 查询多少条数据
+     * @param rowIndex       从第几页开始
+     * @param pageSize       查询多少条数据
      * @return 返回分页后的奖品信息
      */
     List<Award> queryAwardList(@Param("awardCondition") Award awardCondition, @Param("rowIndex") int rowIndex,
-                               @Param("pageSize") int pageSize );
+                               @Param("pageSize") int pageSize);
 
     /**
      * 查询总数量
+     *
      * @param awardCondition 奖品信息
      * @return 返回奖品的总数量
      */
@@ -33,13 +35,15 @@ public interface AwardDao {
 
     /**
      * 根据奖品ID查询对应的奖品信息
+     *
      * @param awardId 奖品ID
      * @return 返回对应的奖品信息
      */
-    Award queryAwardByAwardId(Long awardId);
+    Award queryAwardByAwardId(long awardId);
 
     /**
      * 添加奖品
+     *
      * @param award 奖品信息
      * @return 返回结果 1：成功 0：失败
      */
@@ -47,13 +51,15 @@ public interface AwardDao {
 
     /**
      * 更新奖品
+     *
      * @param award 奖品信息
      * @return 返回结果 1：成功 0：失败
      */
     int updateAward(Award award);
 
     /**
-     * 下架奖品
+     * 删除奖品信息
+     *
      * @param awardId 奖品信息
      * @param shopId
      * @return 返回结果 1：成功 0：失败
