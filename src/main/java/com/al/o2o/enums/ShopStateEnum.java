1dbee5e2acb8b243e45a2ca84d87f14dc02949e2
alex <17674745183@163.com>
2021-10-17 19:49:16 +0800

    格式化代码

--- src/main/java/com/al/o2o/enums/ShopStateEnum.java
+++ src/main/java/com/al/o2o/enums/ShopStateEnum.java
@@ -7,8 +7,8 @@ public enum ShopStateEnum {
     /**
      *
      */
-    CHECK(0,"审核中"),OFFLINE(-1,"非法店铺"),SUCCESS(1,"操作成功"),
-    PASS(2,"通过验证"),INNER_ERROR(-1001,"内部系统错误"),NULL_SHOP(-1003,"shop信息为空");
+    CHECK(0, "审核中"), OFFLINE(-1, "非法店铺"), SUCCESS(1, "操作成功"),
+    PASS(2, "通过验证"), INNER_ERROR(-1001, "内部系统错误"), NULL_SHOP(-1003, "shop信息为空");
 
     private int state;
     private String stateInfo;
@@ -26,11 +26,12 @@ public enum ShopStateEnum {
         this.stateInfo = stateInfo;
 
     }
+
     /**
      * 依据传入的state返回相应的enum值
      */
-    public static ShopStateEnum stateOf(int state){
-        for (ShopStateEnum stateEnum :values()){
+    public static ShopStateEnum stateOf(int state) {
+        for (ShopStateEnum stateEnum : values()) {
             if (stateEnum.getState() == state) {
                 return stateEnum;
             }
