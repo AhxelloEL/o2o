259be4bb7c82c7c00e5a25eb6a9d6eee448deaa8
alex <17674745183@163.com>
2021-10-21 10:49:41 +0800

    格式化代码，添加注释

--- src/main/java/com/al/o2o/dao/ShopAuthMapDao.java
+++ src/main/java/com/al/o2o/dao/ShopAuthMapDao.java
@@ -15,23 +15,26 @@ import java.util.List;
 public interface ShopAuthMapDao {
     /**
      * 分页列出店铺下的授权信息
-     * @param shopId 店铺ID
+     *
+     * @param shopId    店铺ID
      * @param pageIndex 从第几行开始
      * @param pageSize  返回多少行数据
      * @return 返回店铺授权信息
      */
-    List<ShopAuthMap> queryShopAuthMapByShopId(@Param("shopId")long shopId,@Param("pageIndex")int pageIndex,
-                                               @Param("pageSize")int pageSize);
+    List<ShopAuthMap> queryShopAuthMapByShopId(@Param("shopId") long shopId, @Param("pageIndex") int pageIndex,
+                                               @Param("pageSize") int pageSize);
 
     /**
      * 查询店铺授权总数量
+     *
      * @param shopId 店铺ID
      * @return 店铺授权数量
      */
-    int queryShopAuthCountByShopId(@Param("shopId")long shopId);
+    int queryShopAuthCountByShopId(@Param("shopId") long shopId);
 
     /**
      * 授权
+     *
      * @param shopAuthMap
      * @return 返回0：授权失败 1：授权成功
      */
@@ -39,6 +42,7 @@ public interface ShopAuthMapDao {
 
     /**
      * 更新授权信息
+     *
      * @param shopAuthMap
      * @return 返回0:更新失败 1：更新成功
      */
@@ -46,6 +50,7 @@ public interface ShopAuthMapDao {
 
     /**
      * 对某个员工除权
+     *
      * @param shopAuthMap
      * @return 返回0：除权失败 1：除权成功
      */
@@ -53,6 +58,7 @@ public interface ShopAuthMapDao {
 
     /**
      * 根据shopAuthId查询店铺员工
+     *
      * @param shopAuthId 店铺授权主键
      * @return 返回店铺员工权限
      */
