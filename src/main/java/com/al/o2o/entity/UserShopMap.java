e2248f0b284e91fbec99de51c1677524c589bba5
alex <17674745183@163.com>
2021-10-22 21:22:58 +0800

    顾客店铺积分映射 1.添加字段注释 2.格式化代码

--- src/main/java/com/al/o2o/entity/UserShopMap.java
+++ src/main/java/com/al/o2o/entity/UserShopMap.java
@@ -6,14 +6,29 @@ import java.util.Date;
  * @author Xiahuicheng
  * @PackageName:com.al.o2o.entity
  * @ClassName:UserShopMap
- * @Description 顾客店铺积分
+ * @Description 顾客店铺积分映射
  * @date2021/8/24 12:23
  */
 public class UserShopMap {
+    /**
+     * 主键Id
+     */
     private Long userShopId;
+    /**
+     * 创建时间
+     */
     private Date createTime;
+    /**
+     * 积分
+     */
     private Integer point;
+    /**
+     * 用户
+     */
     private PersonInfo user;
+    /**
+     * 店铺
+     */
     private Shop shop;
 
     public Long getUserShopId() {
