8e11354d46f2c76463927031e8d9bb2f9aff931b
alex <17674745183@163.com>
2021-10-23 12:17:01 +0800

    添加字段注释

--- src/main/java/com/al/o2o/entity/ProductSellDaily.java
+++ src/main/java/com/al/o2o/entity/ProductSellDaily.java
@@ -10,10 +10,25 @@ import java.util.Date;
  * @date2021/8/24 11:13
  */
 public class ProductSellDaily {
+    /**
+     * 主键Id
+     */
     private Integer productSellDailyId;
+    /**
+     * 创建时间
+     */
     private Date createTime;
+    /**
+     * 消费商品数量
+     */
     private Integer total;
+    /**
+     * 关联商品信息
+     */
     private Product product;
+    /**
+     * 关联店铺信息
+     */
     private Shop shop;
 
     public Integer getProductSellDailyId() {
