66509e109e80e9668ca5846b9178460303bfa144
alex <17674745183@163.com>
2021-11-02 11:11:33 +0800

    用户在店铺消费映射枚举类

---
+++ src/main/java/com/al/o2o/enums/UserShopMapStateEnum.java
@@ -0,0 +1,40 @@
+package com.al.o2o.enums;
+
+/**
+ * 用户在店铺消费映射枚举类
+ * @author yunSun
+ */
+
+public enum UserShopMapStateEnum {
+	/**
+	 *
+	 */
+	SUCCESS(1, "操作成功"), INNER_ERROR(-1001, "操作失败"), NULL_USERSHOP_ID(-1002, "UserShopId为空"), NULL_USERSHOP_INFO(-1003,
+			"传入了空的信息");
+
+	private int state;
+
+	private String stateInfo;
+
+	private UserShopMapStateEnum(int state, String stateInfo) {
+		this.state = state;
+		this.stateInfo = stateInfo;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public String getStateInfo() {
+		return stateInfo;
+	}
+
+	public static UserShopMapStateEnum stateOf(int index) {
+		for (UserShopMapStateEnum state : values()) {
+			if (state.getState() == index) {
+				return state;
+			}
+		}
+		return null;
+	}
+}
